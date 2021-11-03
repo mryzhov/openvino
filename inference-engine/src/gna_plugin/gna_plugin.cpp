dce0ea3146406effe55035291d9d225623e79f91
mryzhov <mikhail.ryzhov@intel.com>
2021-11-03 12:47:31 +0300

    compact off

--- inference-engine/src/gna_plugin/gna_plugin.cpp
+++ inference-engine/src/gna_plugin/gna_plugin.cpp
@@ -990,7 +990,7 @@ void GNAPlugin::LoadNetwork(CNNNetwork & _network) {
         gnamem->reserve_ptr(nullptr, &pParallelExecutionData, gnamem->getRWBytes() * (gnaFlags->gna_lib_async_threads_num - 1), 64);
     }
 
-    gnaFlags->compact_mode = true;
+    gnaFlags->compact_mode = false;
     gnamem->commit(gnaFlags->compact_mode);
 
     dnn->Init(gnamem->getBasePtr(),
