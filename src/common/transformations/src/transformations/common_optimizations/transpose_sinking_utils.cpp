cab1b42e38868c5ded27c76c3530917cea7dbae5
Evgeny Kotov <evgeny.kotov@intel.com>
2023-03-06 15:39:12 +0100

    fix utils StridedSlice to be sinkable

--- src/common/transformations/src/transformations/common_optimizations/transpose_sinking_utils.cpp
+++ src/common/transformations/src/transformations/common_optimizations/transpose_sinking_utils.cpp
@@ -293,6 +293,7 @@ bool CanPropagateForwardThrough(Node* node) {
     CHECK_TRANSPOSE_SINKING_SUPPORTED(Transpose, node);
     CHECK_TRANSPOSE_SINKING_SUPPORTED(PRelu, node);
     CHECK_TRANSPOSE_SINKING_SUPPORTED(FakeQuantize, node);
+    CHECK_TRANSPOSE_SINKING_SUPPORTED(StridedSlice, node);
 
     return false;
 }
