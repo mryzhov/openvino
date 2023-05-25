220fdbb5384b211b7064a7d27c43efa0572317bc
Evgeny Kotov <evgeny.kotov@intel.com>
2023-05-25 17:30:30 +0200

    add ts_concat_tests

--- src/plugins/intel_gna/tests/unit/transformations/gather_sinking_fuse_test.cpp
+++ src/plugins/intel_gna/tests/unit/transformations/gather_sinking_fuse_test.cpp
@@ -17,6 +17,7 @@
 #include "transformations/gather_sinking_split.hpp"
 #include "transformations/gather_sinking_reshape.hpp"
 #include "transformations/gather_sinking.hpp"
+#include "transformations/ts_concat.hpp"
 
 #include "ngraph/pass/visualize_tree.hpp"  // DEBUG
 
@@ -423,6 +424,63 @@ TEST(GatherSinkingGeneral, General) {
         auto gather2 = MakeGather(sinh, GatherForward, /* axis */ 1);
 
         const auto result = std::make_shared<Result>(gather2);
+        reference_function = std::make_shared<Model>(OutputVector{result}, ParameterVector{input_params1, input_params2});
+    }
+
+    const FunctionsComparator func_comparator =
+        FunctionsComparator::with_default().enable(FunctionsComparator::ATTRIBUTES);
+    const FunctionsComparator::Result result = func_comparator(function, reference_function);
+    ASSERT_TRUE(result.valid) << result.message;
+}
+
+std::vector<size_t> TSConcat_Forward_indexes(size_t size, size_t initial_value) {
+    return std::vector<size_t>{0, 4, 1, 5, 2, 6, 3, 7, 8, 12, 9, 13, 10, 14, 11, 15, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32};
+}
+
+TEST(TSConcat, Forward) {
+    std::shared_ptr<Model> function;
+    {
+        auto input_params1 = std::make_shared<Parameter>(element::Type_t::f32, Shape{2,2,2,2});
+        auto input_params2 = std::make_shared<Parameter>(element::Type_t::f32, Shape{2,2,2,2});
+
+        auto transpose_const = Constant::create(ngraph::element::i64,
+                                                            ngraph::Shape{4},
+                                                            {0,2,3,1});
+
+        auto transpose = std::make_shared<Transpose>(input_params1, transpose_const);
+
+        auto concat = std::make_shared<Concat>(NodeVector{transpose, input_params2}, 0);
+
+        const auto result = std::make_shared<Result>(concat);
+        function = std::make_shared<Model>(OutputVector{result}, ParameterVector{input_params1, input_params2});
+    }
+
+    std::shared_ptr<Model> orig_function = function->clone();
+    ov::pass::Manager manager;
+    manager.register_pass<ov::pass::InitNodeInfo>();
+    manager.register_pass<ov::intel_gna::pass::TSConcatForward>();
+    manager.run_passes(function);
+    ASSERT_NO_THROW(check_rt_info(function));
+
+    std::shared_ptr<Model> reference_function;
+    {
+        auto input_params1 = std::make_shared<Parameter>(element::Type_t::f32, Shape{2,2,2,2});
+        auto input_params2 = std::make_shared<Parameter>(element::Type_t::f32, Shape{2,2,2,2});
+
+        auto reshape_const1 = Constant::create(ngraph::element::i64, ov::Shape{2}, ov::Shape{1,16});
+        auto reshape1 = std::make_shared<Reshape>(input_params1, reshape_const1, false);
+
+        auto reshape_const2 = Constant::create(ngraph::element::i64, ov::Shape{2}, ov::Shape{1,16});
+        auto reshape2 = std::make_shared<Reshape>(input_params2, reshape_const2, false);
+
+        auto concat = std::make_shared<Concat>(NodeVector{reshape1, reshape2}, 1);
+
+        auto gather = MakeGather(concat, TSConcat_Forward_indexes, /* axis */ 1);
+
+        auto reshape_const3 = Constant::create(ngraph::element::i64, ov::Shape{4}, ov::Shape{4,2,2,2});
+        auto reshape3 = std::make_shared<Reshape>(gather, reshape_const3, false);
+
+        const auto result = std::make_shared<Result>(reshape3);
         reference_function = std::make_shared<Model>(OutputVector{result}, ParameterVector{input_params1, input_params2});
     }
 
