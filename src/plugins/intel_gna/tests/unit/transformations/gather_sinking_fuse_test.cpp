c54355f5abbda9e1a1ddede993120a16191521ce
Evgeny Kotov <evgeny.kotov@intel.com>
2023-05-25 15:29:23 +0200

    add tests

--- src/plugins/intel_gna/tests/unit/transformations/gather_sinking_fuse_test.cpp
+++ src/plugins/intel_gna/tests/unit/transformations/gather_sinking_fuse_test.cpp
@@ -15,6 +15,8 @@
 #include "transformations/transpose_nchw.hpp"
 #include "transformations/gather_sinking_matmul.hpp"
 #include "transformations/gather_sinking_split.hpp"
+#include "transformations/gather_sinking_reshape.hpp"
+#include "transformations/gather_sinking.hpp"
 
 #include "ngraph/pass/visualize_tree.hpp"  // DEBUG
 
@@ -330,6 +332,98 @@ TEST(GatherSinkingSplit, Backward) {
 
         const auto result = std::make_shared<Result>(split2);
         reference_function = std::make_shared<Model>(OutputVector{result}, ParameterVector{input_params});
+    }
+
+    const FunctionsComparator func_comparator =
+        FunctionsComparator::with_default().enable(FunctionsComparator::ATTRIBUTES);
+    const FunctionsComparator::Result result = func_comparator(function, reference_function);
+    ASSERT_TRUE(result.valid) << result.message;
+}
+
+TEST(GatherSinkingReshape, Backward) {
+    std::shared_ptr<Model> function;
+    {
+        auto input_params = std::make_shared<Parameter>(element::Type_t::f32, Shape{1,168});
+
+        auto reshape_const1 = Constant::create(ngraph::element::i64, ov::Shape{4}, ov::Shape{1,168,1,1});
+        auto reshape1 = std::make_shared<Reshape>(input_params, reshape_const1, false);
+
+        auto reshape_const2 = Constant::create(ngraph::element::i64, ov::Shape{5}, ov::Shape{1,168,1,1,1});
+        auto reshape2 = std::make_shared<Reshape>(reshape1, reshape_const2, false);
+
+        auto gather = MakeGather(reshape2, GatherForward, /* axis */ 1);
+
+        const auto result = std::make_shared<Result>(gather);
+        function = std::make_shared<Model>(OutputVector{result}, ParameterVector{input_params});
+    }
+
+    std::shared_ptr<Model> orig_function = function->clone();
+    ov::pass::Manager manager;
+    manager.register_pass<ov::pass::InitNodeInfo>();
+    manager.register_pass<ov::intel_gna::pass::GatherSinkingReshapeBackward>();
+    manager.run_passes(function);
+    ASSERT_NO_THROW(check_rt_info(function));
+
+    std::shared_ptr<Model> reference_function;
+    {
+        auto input_params = std::make_shared<Parameter>(element::Type_t::f32, Shape{1,168});
+
+        auto gather = MakeGather(input_params, GatherForward, /* axis */ 1);
+
+        auto reshape_const1 = Constant::create(ngraph::element::i64, ov::Shape{4}, ov::Shape{1,168,1,1});
+        auto reshape1 = std::make_shared<Reshape>(gather, reshape_const1, false);
+
+        auto reshape_const2 = Constant::create(ngraph::element::i64, ov::Shape{5}, ov::Shape{1,168,1,1,1});
+        auto reshape2 = std::make_shared<Reshape>(reshape1, reshape_const2, false);
+
+        const auto result = std::make_shared<Result>(reshape2);
+        reference_function = std::make_shared<Model>(OutputVector{result}, ParameterVector{input_params});
+    }
+
+    const FunctionsComparator func_comparator =
+        FunctionsComparator::with_default().enable(FunctionsComparator::ATTRIBUTES);
+    const FunctionsComparator::Result result = func_comparator(function, reference_function);
+    ASSERT_TRUE(result.valid) << result.message;
+}
+
+TEST(GatherSinkingGeneral, General) {
+    std::shared_ptr<Model> function;
+    {
+        auto input_params1 = std::make_shared<Parameter>(element::Type_t::f32, Shape{20,20});
+        auto input_params2 = std::make_shared<Parameter>(element::Type_t::f32, Shape{20,20});
+        
+        auto gather = MakeGather(input_params1, GatherForward, /* axis */ 1);
+
+        auto tanh = std::make_shared<Tanh>(input_params2);
+        auto mult = std::make_shared<Multiply>(gather, tanh);
+        auto sinh = std::make_shared<Sinh>(mult);
+
+        const auto result = std::make_shared<Result>(sinh);
+        function = std::make_shared<Model>(OutputVector{result}, ParameterVector{input_params1, input_params2});
+    }
+
+    std::shared_ptr<Model> orig_function = function->clone();
+    ov::pass::Manager manager;
+    manager.register_pass<ov::pass::InitNodeInfo>();
+    manager.register_pass<ov::intel_gna::pass::GatherSinkingGeneral>();
+    manager.run_passes(function);
+    ASSERT_NO_THROW(check_rt_info(function));
+
+    std::shared_ptr<Model> reference_function;
+    {
+        auto input_params1 = std::make_shared<Parameter>(element::Type_t::f32, Shape{20,20});
+        auto input_params2 = std::make_shared<Parameter>(element::Type_t::f32, Shape{20,20});
+        
+        auto gather1 = MakeGather(input_params2, GatherBackward, /* axis */ 1);
+
+        auto tanh = std::make_shared<Tanh>(gather1);
+        auto mult = std::make_shared<Multiply>(input_params1, tanh);
+        auto sinh = std::make_shared<Sinh>(mult);
+
+        auto gather2 = MakeGather(sinh, GatherForward, /* axis */ 1);
+
+        const auto result = std::make_shared<Result>(gather2);
+        reference_function = std::make_shared<Model>(OutputVector{result}, ParameterVector{input_params1, input_params2});
     }
 
     const FunctionsComparator func_comparator =
