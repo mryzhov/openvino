83f16fbbf5c3429c11ebf217dc5a9a6c93746e34
Evgeny Kotov <evgeny.kotov@intel.com>
2023-05-29 16:18:46 +0200

    add comments to GatherSinking transformations

--- src/plugins/intel_gna/src/transformations/transpose_nchw.hpp
+++ src/plugins/intel_gna/src/transformations/transpose_nchw.hpp
@@ -11,7 +11,23 @@ namespace intel_gna {
 namespace pass {
 
 /**
- * @brief TODO
+ * @brief Substitutes ngraph::Convolution (NCHW) by GNA-specific GNAConvolution (NHWC)
+ * from
+ *         Any #1
+ *           |
+ *   ngraph::Convolution (NCHW)
+ *           |
+ *        Any #2
+ * to
+ *         Any #1
+ *           |
+ *  Transpose NCHW -> NHWC
+ *           |
+ *    GNAConvolution NHWC
+ *           |
+ *  Transpose NHWC -> NCHW
+ *           |
+ *        Any #2
  */
 class SubstituteGNAConvolution : public ngraph::pass::MatcherPass {
 public:
@@ -19,12 +35,34 @@ public:
     SubstituteGNAConvolution();
 };
 
+/**
+ * @brief Substitutes ngraph::MaxPool (NCHW) by GNA-specific GNAMaxPool (NHWC)
+ * from
+ *         Any #1
+ *           |
+ *   ngraph::MaxPool (NCHW)
+ *           |
+ *        Any #2
+ * to
+ *         Any #1
+ *           |
+ *  Transpose NCHW -> NHWC
+ *           |
+ *    GNAMaxPool NHWC
+ *           |
+ *  Transpose NHWC -> NCHW
+ *           |
+ *        Any #2
+ */
 class SubstituteGNAMaxPool : public ngraph::pass::MatcherPass {
 public:
     NGRAPH_RTTI_DECLARATION;
     SubstituteGNAMaxPool();
 };
 
+/** @brief Calls SubstituteGNAConvolution and SubstituteGNAMaxPool transformations
+ * 
+*/
 class TransposeNCHW : public ngraph::pass::FunctionPass {
 public:
     NGRAPH_RTTI_DECLARATION;
