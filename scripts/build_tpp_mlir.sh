09bba706e76e300985484bb193dc6ab8aa389046
Sergey Lyalin <sergey.lyalin@intel.com>
2024-07-19 17:17:54 +0000

    Reference build scripts for MLIR and TPP-MLIR

---
+++ scripts/build_tpp_mlir.sh
@@ -0,0 +1,12 @@
+# run it from tpp-mlir/build subdirectory
+# set CUSTOM_LLVM_ROOT to llvm-project/build directory
+
+cmake -G Ninja .. \
+   -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+   -DMLIR_DIR=$CUSTOM_LLVM_ROOT/lib/cmake/mlir \
+   -DLLVM_EXTERNAL_LIT=$CUSTOM_LLVM_ROOT/bin/llvm-lit \
+   -DCMAKE_C_COMPILER=clang \
+   -DCMAKE_CXX_COMPILER=clang++ \
+   -DLLVM_USE_LINKER=lld
+
+cmake --build . --target check-tpp
